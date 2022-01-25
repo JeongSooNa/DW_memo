4ba0ef77886cde405c6bc995c852ee019de5a5d6
JeongSooNa <skwjdtn3171@naver.com>
2022-01-25 12:03:30 +0900

    feat : 80점(배열생성풀이)

--- Programmers/체육복.java
+++ Programmers/체육복.java
@@ -17,57 +17,39 @@ public class 체육복 {
 		return result;
 	} // 성공 (65.0점)
 	
-	// 다시 이해! 여벌이 있는 학생도 도난당했을 경우 생각!!!
-	
 	//나누어서! 도난당한학생/빌려주는학생
-	public int solution1(int n, int[] lost, int[] reserve) {
-		if(n<2||n>30) return 0;
-		int result = n-lost.length;
-		// 도난o,여벌o, 중복x 확인 code (Programmers 에서 영향x)
-		if(lost.length==0||lost.length>n) return 0;
-		for(int i=0;i<lost.length;i++) {
-			for(int j=i+1;j<lost.length;j++) {
-				if(lost[i]==lost[j]) return 0;
-			}
-		}
-		if(reserve.length==0||reserve.length>n) return 0;
-		for(int i=0;i<reserve.length;i++) {
-			for(int j=i+1;j<reserve.length;j++) {
-				if(reserve[i]==reserve[j]) return 0;
-			}
-		}
-		
-		int count = 0;
-		for(int i=0;i<lost.length;i++) {
-			for(int j=0;j<reserve.length;j++) {
-				if(lost[i]==reserve[j]+1 || lost[i]==reserve[j]-1) {
-					count++;
-				}
-			}
-		}
-		
-		return 0;
-	}
-	public int solution1(int n, int[] lost, int[] reserve) {
+	public static int solution1(int n, int[] lost, int[] reserve) {
 		// 여분있는 학생이 도난
 		// stu[] 에 -1,0,1
 		int stu[] = new int[n];
+		int count = 0;
 		for(int l : lost) stu[l-1]--;
 		for(int r : reserve) stu[r-1]++;
-		for(int i : lost) {
-			for(int j : reserve) {
-				if(lost[i]==reserve[j]) reserve[j] = -1;
+		
+		for(int i=0;i<lost.length;i++) {
+			for(int j=0;j<reserve.length;j++) {
+				if(lost[i]==reserve[j]-1 || lost[i]==reserve[j]+1) {
+					stu[lost[i]-1]++;
+					stu[reserve[j]-1]--;
+					reserve[j] = -2;
+					break;
+				}
 			}
 		}
-		for(int i : lost) {
-			for(int j : reserve) {
-				
-			}
-		} // 이게 아닌데.............
-		
-		return 0;
-	}
-	//( 인데 배열만들어서 해보자)
+		for(int i : stu) {
+			if(i >= 0) count++;
+		}
 
+		
+		return count; 
+	} //성공 80점....
+	
+	public static void main(String[] args) {
+		int n = 10;
+		int[] lost = {1,3,5,9};
+		int[] reserve = {4,8};
+		int result = solution1(n,lost,reserve);
+		System.out.println(result);
+	}
 }
 // https://programmers.co.kr/learn/courses/30/lessons/42862
