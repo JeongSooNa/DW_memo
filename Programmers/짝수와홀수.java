23882f20b53173063b5c602cda4ff32ec85c6cf2
JeongSooNa <skwjdtn3171@naver.com>
2022-01-26 12:14:28 +0900

    feat : 100점! (인데 수학적오류)

--- Programmers/짝수와홀수.java
+++ Programmers/짝수와홀수.java
@@ -3,10 +3,16 @@ package Programmers;
 public class 짝수와홀수 {
 	public String solution(int num) {
         String answer = "";
-        if(num>=0) {
-        	if(num%2==0) answer="Even";
-        	if(num%2==1) answer="Odd";
-        }
-        return answer;
+        if(num%2==0){
+          answer="Even";
+          return answer;
+        }else{
+          answer="Odd";
+          return answer;
+        } 
+        // if 2개를 쓰면 100점이 안나오는데
+        // if else를 쓰면 100점!
+        // 왜지? 나머지가 음수일 경우나 다른 경우의 수가 있을까?
+        // 수학자한테 물어보면 될 것 같다.
     }
 }
