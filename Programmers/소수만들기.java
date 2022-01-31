1b2cc4d812c770074350c667ae538473e604114d
JeongSooNa <skwjdtn3171@naver.com>
2022-01-31 15:56:50 +0900

    feat : size_t? generic?

---
+++ Programmers/소수만들기.java
@@ -0,0 +1,14 @@
+package programmers;
+
+class 소수만들기{
+
+	int solution(int nums[], int nums_len) {
+	    int answer = -1;
+	    return answer;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
