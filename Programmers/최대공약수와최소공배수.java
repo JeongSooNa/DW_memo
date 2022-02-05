2be87c9778d5a939cefa863eac23d40ca3a2c586
JeongSooNa <skwjdtn3171@naver.com>
2022-02-05 20:22:28 +0900

    feat : 100점!

--- Programmers/최대공약수와최소공배수.java
+++ Programmers/최대공약수와최소공배수.java
@@ -1,35 +1,26 @@
-package Programmers;
+package programmers;
 
 public class 최대공약수와최소공배수 {
 	public static int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int nNum[] = new int[n-1];
-        int mNum[] = new int[m-1];
-        for(int i=1;i<n;i++) {
-        	if(n%i==0) {
-        		nNum[i-1] =i;
+        int[] n1 = new int[n];
+        int[] m1 = new int[m];
+        for(int i=1;i<=n;i++) {
+        	if(n%i==0) n1[i-1]=i;
+        }
+        for(int i=1;i<=n;i++) {
+        	if(m%i==0) m1[i-1]=i;
+        }
+        for(int i : n1) {
+        	for(int j : m1) {
+        		if(i==j && (i!=0||j!=0)) answer[0]=i;
         	}
         }
-        for(int i=1;i<m;i++) {
-        	if(m%i==0) {
-        		mNum[i-1] =i;
-        	}
-        }
-        for(int i=0;i<n-1;i++) {
-        	System.out.println(nNum[i]);
-        }
-        for(int i=0;i<n-1;i++) {
-        	for(int j=0;j<m-1;j++) {
-        		if(nNum[i]==mNum[j] && nNum[i]!=0) answer[0]=nNum[i];
-        	}
-        }
-        
-        
-        
+        answer[1]=n*m/answer[0];
         
         return answer;
     }
-	// 소인수분해 로직을 짜야하나...
+	//  최소공배수 = n * m / 최대공약수
 	
 	//main
 	public static void main(String[] args) {
