0f315c14e6a044c7e4479497e90985f653e4c874
JeongSooNa <skwjdtn3171@naver.com>
2022-02-08 08:51:00 +0900

    feat : 100점!

---
+++ Programmers/두정수사이의합.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+public class 두정수사이의합 {
+	public long solution(int a, int b) {
+        long answer = 0;
+        int tmp = 0;
+        if(a>b) {
+        	tmp=a;
+        	a=b;
+        	b=tmp;
+        }
+        for(int i=a;i<=b;i++) {
+        	answer +=i;
+        }
+        return answer;
+    }
+}
