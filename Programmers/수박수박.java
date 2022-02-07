4270561ae309b05f41ea09fc10fd8ce95783e5c1
JeongSooNa <skwjdtn3171@naver.com>
2022-02-08 08:47:08 +0900

    feat : 100점!

---
+++ Programmers/수박수박.java
@@ -0,0 +1,12 @@
+package Programmers;
+
+public class 수박수박 {
+	public String solution(int n) {
+        String answer = "";
+        for(int i=0;i<n;i++) {
+        	if(i%2==0) answer += "수";
+        	else answer +="박";
+        }
+        return answer;
+    }
+}
