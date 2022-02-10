67831f04225af99a2e462fe37c67f4c4bcc82856
JeongSooNa <skwjdtn3171@naver.com>
2022-02-10 14:41:20 +0900

    feat : 로직은 맞는데..error

--- Programmers/같은숫자는싫어.java
+++ Programmers/같은숫자는싫어.java
@@ -2,30 +2,30 @@ package Programmers;
 
 public class 같은숫자는싫어 {
 	public static int[] solution(int []arr) {
-//		배열 arr의 크기(배열 길이) : 1,000,000 이하의 자연수
-//		배열 arr의 원소(인덱스 값)의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
-		
-        int len = arr.length;
-        int count = 0;
-        int check[] = new int[len];  
-        for(int i=0;i<len-1;i++) {
-        	if(arr[i]==arr[i+1])count++;
-        }
-        int[] answer = new int[count];
-        for(int i=0;i<len-count;i++) {
-        	for(int j=0;j<len-1;j++) {
-        		if(arr[j]!=arr[j+1]) answer[i]=arr[j];
-        		
-        	}
-        	
-        }
-        // 이걸 카운트 하면서 이중for문을 돌리거나 해야 되는데..
-        
+		int len = arr.length;
+		int count = 0;
+		for(int i=1;i<len;i++) {
+			if(arr[i-1]==arr[i]) count++;
+		}
+		int[] answer = new int[len-count];
+		int count1 = 0;
+		for(int i=0;i<(len-count1);i++) {
+			for(int j=count1;j<len-1;j++) {
+				answer[i]=arr[j];
+				count1++;
+				if(answer[i]!=arr[j+1]) break;
+			}
+		}
+		answer[len-count-1] = arr[len-1];
         return answer;
+        
+        //로직은 맞는데 44433 이게 에러가 뜬다.. 머지 배열?for문 문제?
     }
 	public static void main(String[] args) {
-		int[] arr = {4,4,4,3};
-//		int result = solution(arr);
-//		System.out.println(result);
+		int[] arr = {1,1,3,3,0,1,1};
+		int result[] = solution(arr);
+		for(int i : result) {
+			System.out.println(i);
+		}
 	}
 }
