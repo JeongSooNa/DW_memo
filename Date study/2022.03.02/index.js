97b3d0ed7ee8c8fb7922ce0f9990b14f2dbe23e1
JeongSooNa <skwjdtn3171@naver.com>
2022-03-02 16:27:18 +0900

    feat : nodeJS

--- Date study/2022.03.02/index.js
+++ Date study/2022.03.02/index.js
@@ -36,3 +36,38 @@ if(age>=30){
 else{
     console.log(name+"는 20대입니다.")
 }
+
+// 상수 선언(java에서의 final)
+const pi = 3.14
+// 배열 생성 및 index
+var array = [1,2,3,4,5,10]
+for(let i=0;i<array.length;i++){
+    if(array[i]%2==0){
+        var value = array[i]
+        console.log(value+"는 짝수입니다.")  
+    } 
+}
+var max = 0
+for(let i=0;i<array.length;i++){
+    var value = array[i]
+    if(max<value) max = value
+}
+console.log("max : "+max)
+
+// 배열로 table 만들기
+var data = [
+    {name : "son", position : "Forward", backNum : 7},
+    {name : "Cha", position : "Forward", backNum : 10},
+    {name : "Park", position : "Midfielder", backNum : 7}
+]
+console.log(data[0].name)
+console.log(data[1].name)
+console.log(data[2].name)
+var name = []
+for(let i=0;i<data.length;i++){
+    name[i] = data[i].name
+}
+console.log(name)
+console.log(data[0].name)
+
+console.log(data[1])
