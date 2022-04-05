b925ecc7274be1cb96fdd70eaf1a0f0b2d3f7379
JeongSooNa <skwjdtn3171@naver.com>
2022-04-05 12:32:25 +0900

    feat : java class

--- Date study/2022.04.05-java class, getter, setter/붕어빵/붕어빵장사.java
+++ Date study/2022.04.05-java class, getter, setter/붕어빵/붕어빵장사.java
@@ -10,8 +10,17 @@ public class 붕어빵장사 {
 		
 		붕어빵 boong2 = new 붕어빵();
 		boong2.붕어빵종류 = "슈크림";
-		boong2.붕어빵수량 = 15;
+		boong2.붕어빵수량 = 20;
 		System.out.println("붕어빵종류 : "+boong2.붕어빵종류);
 		System.out.println("붕어빵수량 : "+boong2.붕어빵수량+"개");
+		
+		System.out.println();
+		
+		붕어빵 팥붕어빵 = new 붕어빵("팥",10);
+		붕어빵 슈크림붕어빵 = new 붕어빵("슈크림",20);
+		붕어빵 피자붕어빵 = new 붕어빵("피자",30);
+		System.out.println(팥붕어빵.붕어빵종류+","+팥붕어빵.붕어빵수량);
+		System.out.println(슈크림붕어빵.붕어빵종류+","+슈크림붕어빵.붕어빵수량);
+		System.out.println(피자붕어빵.붕어빵종류+","+피자붕어빵.붕어빵수량);
 	}
 }
