2f3daa887d0fa7d0d39107bdd7e7a1d007b21e35
JeongSooNa <skwjdtn3171@naver.com>
2022-04-20 12:03:53 +0900

    feat : homework

--- Date study/DW-homwork/0418_숙제/JAVA/user_homework_for_interface/MainController.java
+++ Date study/DW-homwork/0418_숙제/JAVA/user_homework_for_interface/MainController.java
@@ -23,27 +23,24 @@ public class MainController{
         System.out.println("**1");
         service.updateSal(list, "DEVELOP");
         for(int i=0;i<list.size();i++) {
-        	System.out.println(list.get(i).getSal());
+        	System.out.println("사원이름 : "+list.get(i).getEname()+" / 부서이름 : "+list.get(i).getDname()+" / 직원급여 : "+list.get(i).getSal()+" / 직원레벨 : "+list.get(i).getLv());
         }
         //2. 급여가 0 ~ 100 : 1 레벨, 101 ~ 1000 : 2 레벨, 1001 이상부터는 3 레벨로 수정하시오.
         System.out.println("**2");
         service.updateLv(list);
         for(int i=0;i<list.size();i++) {
-        	System.out.println(list.get(i).getLv());
+        	System.out.println("사원이름 : "+list.get(i).getEname()+" / 부서이름 : "+list.get(i).getDname()+" / 직원급여 : "+list.get(i).getSal()+" / 직원레벨 : "+list.get(i).getLv());
         }
         //3. 레벨 3이상 직원을 모두 해고하시오.
         System.out.println("**3");
         service.deleteUser(list, 3);
         for(int i=0;i<list.size();i++) {
-        	System.out.println(list.get(i).getEname());
+        	System.out.println("사원이름 : "+list.get(i).getEname()+" / 부서이름 : "+list.get(i).getDname()+" / 직원급여 : "+list.get(i).getSal()+" / 직원레벨 : "+list.get(i).getLv());
         }
         //4. 이름이 King인 직원의 모든 정보 조회.
         System.out.println("**4");
         UserVO king = service.selectUserByEname(list, "King");
-        System.out.println(king.getEname());
-        System.out.println(king.getDname());
-        System.out.println(king.getSal());
-        System.out.println(king.getLv());
+        System.out.println("사원이름 : "+king.getEname()+" / 부서이름 : "+king.getDname()+" / 직원급여 : "+king.getSal()+" / 직원레벨 : "+king.getLv());
         
         	
     }
